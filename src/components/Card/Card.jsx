0d959ebb8f960658746ffd677fbba59f2ef1f722
Ayyappa <s.v.ayyappanaik@gmail.com>
2026-06-30 14:23:39 +0530

    change in the card and card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,14 @@
 import React from "react";
-import Chip from "@mui/material/Chip";
 import styles from "./Card.module.css";
 
-function Card({ image, follows, title }) {
+function Card({ title, subtitle, imageSrc }) {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        <img
-          src={image}
-          alt={title}
-          className={styles.image}
-        />
-
-        <div className={styles.chipContainer}>
-          <Chip
-            label={`${follows} Follows`}
-            size="small"
-            className={styles.chip}
-          />
-        </div>
+    <div className={styles.card}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={imageSrc || "https://placehold.co/200"} alt={title} />
       </div>
-
-      <p className={styles.title}>{title}</p>
+      <h3 className={styles.title}>{title || "Album Title"}</h3>
+      <p className={styles.subtitle}>{subtitle || "Artist Name"}</p>
     </div>
   );
 }
